52da1215a054d471c0b92a8706b2874100156d45
Junzj <jzjtaizi@qq.com>
2017-06-11 13:24:12 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/gson/City.java
@@ -0,0 +1,50 @@
+package com.coolweather.android.gson;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jzjta on 2017/6/11.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
